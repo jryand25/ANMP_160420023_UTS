05f0de6c61c20aa4938e087a3d7de77931917dbf
jryand25 <jonathan.ryan.darmawan@gmail.com>
2023-05-07 20:55:52 +0700

    init

---
+++ app/src/main/java/com/ryan/anmp_160420023_uts/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ryan.anmp_160420023_uts.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ryan.anmp_160420023_uts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
